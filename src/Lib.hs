da81b7dc1541ac89761d79c9dc644578774ad894
Carlos Gómez <carliros.g@gmail.com>
2018-01-31 17:41:04 -0400

    CHSS-011: Add support for SQLite integration (memory based)

--- src/Lib.hs
+++ src/Lib.hs
@@ -6,35 +6,51 @@ module Lib
     , app
     ) where
 
-import Data.Aeson
-import Data.Aeson.TH
-import Network.Wai
-import Network.Wai.Handler.Warp
-import Servant
-
-data User = User
-  { userId        :: Int
-  , userFirstName :: String
-  , userLastName  :: String
-  } deriving (Eq, Show)
-
-$(deriveJSON defaultOptions ''User)
+import           Control.Monad.IO.Class   (liftIO)
+import           Control.Monad.Logger     (runStderrLoggingT)
+import           Data.Aeson
+import           Data.Aeson.TH
+import           Data.String.Conversions  (cs)
+import           Database.Persist
+import           Database.Persist.Sqlite  (ConnectionPool, createSqlitePool,
+                                           entityVal, insert, runMigration,
+                                           runSqlPersistMPool, runSqlPool,
+                                           selectFirst, selectList, (==.),
+                                           (>=.))
+import           Models
+import           Network.Wai
+import           Network.Wai.Handler.Warp
+import           Servant
 
 type API = "users" :> Get '[JSON] [User]
 
-startApp :: IO ()
-startApp = run 8080 app
+makeApp :: FilePath -> IO Application
+makeApp dbFile
+  = do pool <- runStderrLoggingT $ do createSqlitePool (cs dbFile) 5
+       runSqlPool (preProcessDB) pool
+       return $ app pool
+  where preProcessDB = do runMigration migrateAll
+                          insert $ User (cs "John Doe") 30  --TODO: to be deleted, this is just for testing purposes
+                          insert $ User (cs "Mark Trude") 25  --TODO: to be deleted, this is just for testing purposes
+                          insert $ User (cs "Maria Db") 20  --TODO: to be deleted, this is just for testing purposes
+                          return ()
 
-app :: Application
-app = serve api server
+startApp :: FilePath -> IO ()
+startApp dbFile
+  = run 8080 =<< makeApp dbFile
+
+app :: ConnectionPool -> Application
+app pool = serve api $ server pool
 
 api :: Proxy API
 api = Proxy
 
-server :: Server API
-server = return users
+server :: ConnectionPool -> Server API
+server pool = getAllUsersHandler
+  where getAllUsersHandler :: Handler [User]
+        getAllUsersHandler = liftIO $ getAllUsers
 
-users :: [User]
-users = [ User 1 "Isaac" "Newton"
-        , User 2 "Albert" "Einstein"
-        ]
+        getAllUsers :: IO [User]
+        getAllUsers = flip runSqlPersistMPool pool $ do
+            users <- selectList [UserAge >=. 0] []
+            return $ map entityVal users
